7ed8c307409afbfb92a246a59b1b0fae2db6c81d
Leon Suarez <leonsua@gmail.com>
2025-10-15 11:47:31 -0400

    added axios to the project and created goal type so as created a hook to fetch the goals

---
+++ client/src/hoooks/useFetch.tsx
@@ -0,0 +1,35 @@
+// src/hooks/useFetch.ts
+import { useState, useEffect } from 'react';
+import axios, { type AxiosRequestConfig } from 'axios';
+
+interface FetchState<T> {
+  data: T | null;
+  loading: boolean;
+  error: string | null;
+}
+
+export function useFetch<T>(url: string, options?: AxiosRequestConfig): FetchState<T> {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<T>(url, options);
+        setData(response.data);
+      } catch (err) {
+        if (axios.isAxiosError(err)) {
+          setError(err.message);
+        } else {
+          setError('An unexpected error occurred.');
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url, options]);
+
+  return { data, loading, error };
+}
---
+++ client/src/types/Goal.tsx
@@ -0,0 +1,5 @@
+export interface Goal {
+  _id: string;
+  goal: string;
+  summary: string;
+}
